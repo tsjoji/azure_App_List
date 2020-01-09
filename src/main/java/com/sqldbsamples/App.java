a0cbef35889c7fc340d338654b73fcf5eb454072
Jorge Teixeira <Jbt8@njit.edu>
2020-01-08 20:15:37 -0500

    original code

---
+++ src/main/java/com/sqldbsamples/App.java
@@ -0,0 +1,50 @@
+package com.sqldbsamples;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // Connect to database
+        String hostName = "group-app-db-server.database.windows.net"; // update me
+        String dbName = "group-app-db-name"; // update me
+        String user = "group-app-login"; // update me
+        String password = "Bardop2020"; // update me
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT * FROM ToDoItem";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                // Print results from select statement
+                System.out.println("Top 20 categories:");
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(6) + " "
+                            + resultSet.getString(7));
+                }
+                connection.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
